2af36dfcf7304cdf6836205a29134af8dca27f6e
Ayushgokhe123 <ayush.gokhe@riseslabs.com>
2024-03-10 19:46:59 +0530

    MY Document UI

--- frontend/src/pages/MyDocument.jsx
+++ frontend/src/pages/MyDocument.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const MyDocument = () => {
   return (
-    <div>MyDocument</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <div style={{float:'right'}} className="mb-3">
+            <NavLink to="/create" className="btn btn-success" style={{ textDecoration: "none" }}>
+              Create
+            </NavLink>
+          </div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Sr. No</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>
+                  <NavLink to="/edit" className="btn btn-secondary" style={{ textDecoration: "none" }}>
+                    Edit
+                  </NavLink>
+                  <NavLink to="/view" className="btn btn-success mx-1" style={{ textDecoration: "none" }}>
+                    View
+                  </NavLink>
+                  <NavLink to="/delete" className="btn btn-danger" style={{ textDecoration: "none" }}>
+                    Delete
+                  </NavLink>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MyDocument
+export default MyDocument;
